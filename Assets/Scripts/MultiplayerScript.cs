ff3ac72eb790410d1eb746216cc7a93ed006baca
JamiePhillips370 <131703983+JamiePhillips370@users.noreply.github.com>
2023-06-24 14:52:35 +0200

    h
    h

--- Assets/Scripts/MultiplayerScript.cs
+++ Assets/Scripts/MultiplayerScript.cs
@@ -4,12 +4,34 @@ using UnityEngine.SceneManagement;
 public class MultiplayerScript : MonoBehaviour
 {
     public Camera mainCamera;
+    public GameObject inventory1Prefab; // Prefab of the inventory UI for Player1
+    public GameObject inventory2Prefab; // Prefab of the inventory UI for Player2
 
     private string scene0 = "Scene0";
     private string scene1 = "Scene1";
     private int currentPlayer = 1;
 
     private RenderTexture renderTexture;
+    private GameObject inventory1; // Reference to the instantiated inventory UI for Player1
+    private GameObject inventory2; // Reference to the instantiated inventory UI for Player2
+
+    private void Start()
+    {
+        // Load both scenes
+        LoadScene(scene0);
+        LoadScene(scene1);
+
+        // Set Scene0 as the active scene
+        SceneManager.SetActiveScene(SceneManager.GetSceneByName(scene0));
+
+        // Instantiate the inventory UI for Player1
+        inventory1 = Instantiate(inventory1Prefab);
+        inventory1.SetActive(true);
+
+        // Instantiate the inventory UI for Player2
+        inventory2 = Instantiate(inventory2Prefab);
+        inventory2.SetActive(false);
+    }
 
     private void Update()
     {
@@ -24,13 +46,23 @@ public class MultiplayerScript : MonoBehaviour
     {
         if (currentPlayer == 1)
         {
+            // Disable the inventory UI for Player1
+            inventory1.SetActive(false);
             LoadScene(scene1);
             currentPlayer = 2;
+
+            // Enable the inventory UI for Player2
+            inventory2.SetActive(true);
         }
         else
         {
+            // Disable the inventory UI for Player2
+            inventory2.SetActive(false);
             LoadScene(scene0);
             currentPlayer = 1;
+
+            // Enable the inventory UI for Player1
+            inventory1.SetActive(true);
         }
     }
 
